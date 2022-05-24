3066c602ec27f9601a9cad30e57fd01a970bcec0
Teddy <84819953+teddyscrewthis@users.noreply.github.com>
2022-05-24 11:18:26 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#I have to put the actual process names later. Cant do it rn as I'm not on my desktop.
+
 try:
   from colorama import *
   import psutil
@@ -33,10 +35,73 @@ def main_menu():
   print(ascii_menu)
   print("")
   print("[0] Exit")
-  print("[1] Test Software")
+  print("[1] Custom")
   print("[2] Easy Anti-Cheat")
   print("[3] Battleye")
   print("[4] Richote")
   print("[5] Help")
   
+def check(processName):
+  
+  for proc in psutil.process_iter():
+        try:
+            # Check if process name contains the given name string.
+            if processName.lower() in proc.name().lower():
+                return True
+        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+            pass
+    return False;
+
+def EAC():
+    if check('EasyAntiCheat'):
+      print(Fore.RED + "[!] Easy Anti-Cheat is running [!]" + Fore.RESET)
+      
+      input("Press any key to continue ---> ")
+      return main_menu()
+      
+    else:
+      print(Fore.GREEN + "[!] Easy Anti-Cheat is not running [!]" + Fore.RESET)
+      input("Press any key to continue ---> ")
+      return main_menu()
+  
+def Battleye():
+    if check('Battleye'):
+      print(Fore.RED + "[!] Battleye is running [!]" + Fore.RESET)
+      
+      input("Press any key to continue ---> ")
+      return main_menu()
+      
+    else:
+      print(Fore.GREEN + "[!] Battleye is not running [!]" + Fore.RESET)
+      input("Press any key to continue ---> ")
+      return main_menu()
+    
+def richocet():
+  if check('ricochet'):
+      print(Fore.RED + "[!] ricochet is running [!]" + Fore.RESET)
+      
+      input("Press any key to continue ---> ")
+      return main_menu()
+      
+    else:
+      print(Fore.GREEN + "[!] ricochet is not running [!]" + Fore.RESET)
+      input("Press any key to continue ---> ")
+      return main_menu()
+    
+def custom():
+  test_proc = input("Enter Proc name you want to test ---> ")
+  
+  if check(test_proc):
+      print(Fore.RED + f"[!] {test_proc} is running [!]" + Fore.RESET)
+      
+      input("Press any key to continue ---> ")
+      return main_menu()
+      
+    else:
+      print(Fore.GREEN + f"[!] {test_proc} is not running [!]" + Fore.RESET)
+      input("Press any key to continue ---> ")
+      return main_menu()
+    
+if '__name__' == '__main__':
+  main_menu()
   
