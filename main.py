e602f39d5dc9132f3023e3163d16a9432fd8aea7
Teddy <84819953+teddyscrewthis@users.noreply.github.com>
2022-05-25 09:01:55 -0700

    Update main.py

--- main.py
+++ main.py
@@ -17,90 +17,72 @@ def app_logo():
 ▄▀█ █▄░█ ▀█▀ █ ▄▄ █▀▀ █░█ █▀▀ ▄▀█ ▀█▀   █▀▄ █▀▀ ▀█▀ █▀▀ █▀▀ ▀█▀ █▀█ █▀█
 █▀█ █░▀█ ░█░ █ ░░ █▄▄ █▀█ ██▄ █▀█ ░█░   █▄▀ ██▄ ░█░ ██▄ █▄▄ ░█░ █▄█ █▀▄"""
   print(ascii_menu)
-  
-  
-  
+ 
+
 def menu():
   print("[0] Exit")
-  print("[1] Test Software")
-  print("[2] Easy Anti-Cheat")
-  print("[3] Battleye")
-  print("[4] Richote")
-  print("[5] Help")
+  print("[1] Easy Anti-Cheat")
+  print("[2] Battleye")
+  print("[3] Richote")
+  print("[4] Help")
+  
+  options = inout("Select Anti-Cheat to Check ---> ")
   
 
 def main_menu():
+  os.system("cls")
+  app_logo()
+  menu()
   
-  ascii_menu = """
-▄▀█ █▄░█ ▀█▀ █ ▄▄ █▀▀ █░█ █▀▀ ▄▀█ ▀█▀   █▀▄ █▀▀ ▀█▀ █▀▀ █▀▀ ▀█▀ █▀█ █▀█
-█▀█ █░▀█ ░█░ █ ░░ █▄▄ █▀█ ██▄ █▀█ ░█░   █▄▀ ██▄ ░█░ ██▄ █▄▄ ░█░ █▄█ █▀▄"""
-  print(ascii_menu)
-  print("")
-  print("[0] Exit")
-  print("[1] Custom")
-  print("[2] Easy Anti-Cheat")
-  print("[3] Battleye")
-  print("[4] Richote")
-  print("[5] Help")
-  
-def check(processName):
-  
-  for proc in psutil.process_iter():
-        try:
-            # Check if process name contains the given name string.
-            if processName.lower() in proc.name().lower():
-                return True
-        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
-            pass
-    return False;
+def getService(name):
+
+    service = None
+    try:
+        service = psutil.win_service_get(name)
+        service = service.as_dict()
+    except Exception as ex:
+        print(str(ex))
+    return service
 
 def EAC():
-    if check('EasyAntiCheat'):
-      print(Fore.RED + "[!] Easy Anti-Cheat is running [!]" + Fore.RESET)
-      
-      input("Press any key to continue ---> ")
-      return main_menu()
-      
-    else:
-      print(Fore.GREEN + "[!] Easy Anti-Cheat is not running [!]" + Fore.RESET)
-      input("Press any key to continue ---> ")
-      return main_menu()
   
-def Battleye():
-    if check('Battleye'):
-      print(Fore.RED + "[!] Battleye is running [!]" + Fore.RESET)
-      
+  service = getService('EasyAntiCheat')
+  print(service)
+
+  if service:
+      print(Fore.GREEN + "Easy Anti-Cheat is running" + Fore.RESET)
       input("Press any key to continue ---> ")
       return main_menu()
-      
-    else:
-      print(Fore.GREEN + "[!] Battleye is not running [!]" + Fore.RESET)
+  else:
+      print(Fore.RED + "Easy Anti-Cheat is  not running")
       input("Press any key to continue ---> ")
       return main_menu()
     
-def richocet():
-  if check('ricochet'):
-      print(Fore.RED + "[!] ricochet is running [!]" + Fore.RESET)
-      
+def BATTLEYE():
+  
+  service = getService('Battleye')
+  print(service)
+
+  if service:
+      print(Fore.GREEN + "Battleye is running" + Fore.RESET)
       input("Press any key to continue ---> ")
       return main_menu()
-      
-    else:
-      print(Fore.GREEN + "[!] ricochet is not running [!]" + Fore.RESET)
+  else:
+      print(Fore.RED + "Battleye is  not running")
       input("Press any key to continue ---> ")
       return main_menu()
     
-def custom():
-  test_proc = input("Enter Proc name you want to test ---> ")
+def RICOCHET():
   
-  if check(test_proc):
-      print(Fore.RED + f"[!] {test_proc} is running [!]" + Fore.RESET)
-      
+  service = getService('Ricochet')
+  print(service)
+
+  if service:
+      print(Fore.GREEN + "Ricochet is running" + Fore.RESET)
       input("Press any key to continue ---> ")
       return main_menu()
-      
-    else:
-      print(Fore.GREEN + f"[!] {test_proc} is not running [!]" + Fore.RESET)
+  else:
+      print(Fore.RED + "Ricochet is  not running")
       input("Press any key to continue ---> ")
       return main_menu()
     
